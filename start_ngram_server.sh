915eb9bb29a09908b7bab4e0bb45c0208ff235e6
ra1nbowpill <ygor.gallina@gmail.com>
2017-06-22 17:42:48 +0200

    Ported code to Python3

--- start_ngram_server.sh
+++ start_ngram_server.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 # update with your own paths
-LATTICE_TOOL_DIR=/media/Data/work/Tools/srilm/bin/i686-m64
-LM_DIR=/media/Data/work/Development/EclipseWorkspace/TextCleanser/data/
+# LATTICE_TOOL_DIR="/media/Data/work/Tools/srilm/bin/i686-m64"
+# LM_DIR="/media/Data/work/Development/EclipseWorkspace/TextCleanser/data/"
+LM_DIR="/Users/ygorgallina/Documents/Cours/M1/Stage/Outils/TextCleanser/data/"
+LATTICE_TOOL_DIR="/Users/ygorgallina/Documents/Cours/M1/Stage/Outils/srilm-1.6.0/bin/macosx/"
+
 $LATTICE_TOOL_DIR/ngram -lm $LM_DIR/tweet-lm.gz -mix-lm $LM_DIR/latimes-lm.gz -lambda 0.7 -mix-lambda2 0.3 -server-port 12345 &
--- web_service.py
+++ web_service.py
@@ -8,23 +8,24 @@ Date: Jul 2011
 """
 
 import cherrypy
-from cherrypy import expose
 from cleanser import TextCleanser
 
+
 class CleanserWebService():
-    
+
     def __init__(self):
         self.tc = TextCleanser()
-    
-    @expose
+
+    @cherrypy.expose
     def clean(self, text):
         cleantext, error, replacements = self.tc.ssk_cleanse(text)
-        if error=="":
+        if error == "":
             return cleantext
         else:
             # an error occurred
             return error
 
+
 if __name__ == "__main__":
-    print  "Starting TextCleanser Web Service on localhost"
+    print("Starting TextCleanser Web Service on localhost")
     cherrypy.quickstart(CleanserWebService())
